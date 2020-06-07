852c5d613810da3ca69d90fbad577cc98e66cd4d
Nainika Baliga <nainika@Nainikas-MacBook-Air.local>
2020-06-08 01:20:33 +0530

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# my-portfolio-website
-I hope to showcase a little piece of myself here.
--- src/components/BackDrop.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const BackDrop = (props) => {
-  return <div className="backdrop" onClick={props.click}></div>;
-};
-
-export default BackDrop;
